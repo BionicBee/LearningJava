fe968d0ba5ec95843e4e714206869b0446b54f4b
Brendan Ingham <b.ingham@spartatec.co.uk>
2014-12-21 18:22:14 +0000

    Commit 2

--- src/LearningJava.java
+++ src/LearningJava.java
@@ -2,17 +2,12 @@
     This is a Learning Java Thing!
  */
 
-import java.lang.String;
-import java.lang.System;
-
 public class LearningJava {
     /*
         Prints Hello Laura to Console
      */
-
-
     public static void main(String []args){
-        System.out.println("Hello Laura!");
+        System.out.println("Hello Laura!"); // Prints Hello World!
     }
 
 }

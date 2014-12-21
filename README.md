da4bfd6c53fee56abb0d2210bb3fc57d3d2b0569
Brendan Ingham <b.ingham@spartatec.co.uk>
2014-12-21 18:07:25 +0000

    Hello World!

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ LearningJava
 ============
 
 This is a Learning Java Repository!
+
+Update 1 : Sun 21 Dec : 6:04PM - Hello World!
---
+++ src/LearningJava.java
@@ -0,0 +1,18 @@
+/*
+    This is a Learning Java Thing!
+ */
+
+import java.lang.String;
+import java.lang.System;
+
+public class LearningJava {
+    /*
+        Prints Hello Laura to Console
+     */
+
+
+    public static void main(String []args){
+        System.out.println("Hello Laura!");
+    }
+
+}
